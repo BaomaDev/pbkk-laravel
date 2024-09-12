055aa2ce5896ca16eb107897075f07b5ce940397
lookingfrag <jessesimanjuntaks@gmail.com>
2024-09-12 08:24:40 +0700

    feat : learn data structures in laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman about</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <h1>Halaman about</h1>
+    <h3>{{ $name }}</h3>
+    <img src="img/mantap.png" alt="Gambar about">
+</body>
+</html>
