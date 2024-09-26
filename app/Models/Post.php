eceafcc5c3a7291b321ad5ea8202bf4cd5886c3d
lookingfrag <jessesimanjuntaks@gmail.com>
2024-09-26 07:55:29 +0700

    feat : Database & Migration and Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,45 +2,9 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-Class Post {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'post-title-1',
-                'title' => 'Post Title 1',
-                'author' => 'Author 1',
-                'body' => 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate. Quisquam, voluptate.'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'post-title-2',
-                'title' => 'Post Title 2',
-                'author' => 'Author 2',
-                'body' => 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate. Quisquam, voluptate.'
-            ],
-            [
-                'id' => 3,
-                'slug' => 'post-title-3',
-                'title' => 'Post Title 3',
-                'author' => 'Author 3',
-                'body' => 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate. Quisquam, voluptate.'
-            ]
-            ];
-    }
-
-    public static function find($slug) {
-        // return Arr::first(static::all(), function($post) use ($slug){
-        //     return $post['slug'] == $slug;
-        // });
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-
-        if(!$post) {
-            abort(404);
-        }
-
-        return $post;
-    }
+Class Post extends Model {
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,12 +1,11 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-    <article class="py-8 max-w-screen-md border-b border-gray-300">
-            <h1 class="text-2xl font-bold">{{ $post['title'] }}</h1>
+        <article class="py-8 max-w-screen-md border-b border-gray-300">
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
-        <div class="text-base text-gray-500">
-            <a href="#">{{ $post['author'] }}</a> | 1 Januari 2024
-        </div>
-        <p class="my-4 font-light">{{ $post['body'] }}</p>
-        <a href="/posts" class=" font-medium text-blue-500 hover:underline">&laquo; Back to Posts</a>
-    </article>
+            <div class="text-base text-gray-500">
+                <a href="#">{{ $post['author'] }}</a> | 1 Januari 2024
+            </div>
+            <p class="my-4 font-light">{{ $post['body'] }}</p>
+            <a href="/posts" class=" font-medium text-blue-500 hover:underline">&laquo; Back to Posts</a>
+        </article>
 </x-layout>
